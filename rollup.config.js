49d36e8ebfb5faa32c5fabd07ca92994326d81e1
Alexandra Chernyshova <alexandrachrystal@gmail.com>
2025-04-14 15:07:40 +0700

    updates

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,30 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.js",
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist/cjs",
       format: "cjs",
       sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: "src",
     },
-    {
-      file: "dist/index.es.js",
+    plugins: [resolve(), commonjs(), typescript()],
+    external: ["react", "react-dom", "styled-components", "antd"],
+  },
+
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist/esm", // директория для ESM
       format: "esm",
       sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: "src",
     },
-  ],
-  plugins: [resolve(), commonjs(), typescript()],
-  external: ["react", "react-dom", "styled-components", "antd"],
-};
+    plugins: [resolve(), commonjs(), typescript()],
+    external: ["react", "react-dom", "styled-components", "antd"],
+  },
+];
